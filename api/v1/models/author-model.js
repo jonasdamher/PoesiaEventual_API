1f6bb1eb4816e0ea89e8707d2974656fb14958a9
Jonás Damián <jonas.damher@gmail.com>
2021-04-13 10:16:59 +0100

    change fields names schema mongoose to collection

--- api/v1/models/author-model.js
+++ api/v1/models/author-model.js
@@ -6,6 +6,18 @@ const authorSchema = mongoose.Schema({
     name: {
         type: String,
         min: 2,
+        validate: {
+            validator: function (v) {
+                return /[^0-9]/.test(v);
+            },
+            message: props => `(${props.value}) no tiene el formato adecuado.`
+        },
+        required: [true, 'Es obligatorio introducir un nombre.'],
+        unique: [true, 'El nombre introducido ya existe.']
+    },
+    url: {
+        type: String,
+        min: 2,
         required: true,
         unique: true
     }
