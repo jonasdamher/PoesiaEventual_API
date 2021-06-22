7ac74dc56c4f2e0af2c5a26b6ed93834a8f7864d
jonasdamher <jonas.damher@gmail.com>
2021-06-22 19:17:00 +0100

    UPDATE: widdleware limite de peticiones, limite de kb en body de solicitud

--- src/api/app.ts
+++ src/api/app.ts
@@ -4,8 +4,10 @@ import express, { NextFunction, Request, Response } from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
 import cors from 'cors';
-import limit_mongo from './v1/middlewares/limit-mongo';
 import moment from 'moment';
+
+import limit_mongo from './v1/middlewares/limit-mongo';
+import routes from './v1/routes';
 
 moment.locale('es');
 const app = express();
@@ -13,10 +15,15 @@ const app = express();
 app.use(compression());
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '60kb' }), (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err) {
+        return res.sendStatus(400);
+    }
+    next();
+});
+app.use(express.urlencoded({ extended: false }));
 app.use(limit_mongo);
 
-import routes from './v1/routes';
-app.use('/api/v1', routes)
+app.use('/api/v1', routes);
 
 export default app;
