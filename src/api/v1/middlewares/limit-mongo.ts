ec78cd75dba8b69d26205f12ca0742b4ea2986ab
jonasdamher <jonas.damher@gmail.com>
2021-06-22 19:18:08 +0100

    UPDATE: widdleware limite de peticiones mongo

--- src/api/v1/middlewares/limit-mongo.ts
+++ src/api/v1/middlewares/limit-mongo.ts
@@ -14,12 +14,8 @@ const limiter_mongo = new RateLimiterMongo(opts);
 
 const limiter = (req: Request, res: Response, next: NextFunction) => {
     limiter_mongo.consume(req.ip)
-        .then(() => {
-            next();
-        })
-        .catch(_ => {
-            res.status(429).send('Too Many Requests');
-        });
+        .then(() => next())
+        .catch(() => res.status(429).send('Too Many Requests'));
 };
 
 export default limiter;
