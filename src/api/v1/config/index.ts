e074eab91c2a1f5bf7f4f11b392b9112ba6954a9
jonasdamher <jonas.damher@gmail.com>
2021-06-23 18:04:37 +0100

    UPDATE: prepración main

--- src/api/v1/config/index.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export default {
-    version: process.env.API_VERSION,
-    mongo_uri: process.env.MONGODB_URI ?? '',
-    port: process.env.PORT,
-    node_env:process.env.NODE_ENV
-}
