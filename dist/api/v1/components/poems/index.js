bfb14d777638d0719a16ea373006ddf16a621d82
jonasdamher <jonas.damher@gmail.com>
2021-06-24 10:11:20 +0100

    ADD: dist nueva

---
+++ dist/api/v1/components/poems/index.js
@@ -0,0 +1,34 @@
+'use strict';
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const validation = __importStar(require("./poem-validation"));
+const poems = __importStar(require("./poem-controller"));
+const router = express_1.default.Router();
+router.get('/', validation.getAll, poems.getAll);
+router.get('/get/:id', validation.getWithId, poems.getWithId);
+router.get('/search/:search', validation.searchPoem, poems.searchPoem);
+router.get('/random', poems.random);
+exports.default = router;
+//# sourceMappingURL=index.js.map
