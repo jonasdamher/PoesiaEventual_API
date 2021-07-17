25d24e63f9ee2ab0bf8aa366190527e8f5cea485
jonasdamher <jonas.damher@gmail.com>
2021-07-17 10:26:02 +0100

    ADD: conexión a DB mongo

---
+++ src/api/v2/db/Mongo.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import config from '../config';
+
+class Mongo {
+    async connect(): Promise<Boolean | Error> {
+        return new Promise((resolve, reject) => {
+
+            const options = {
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useUnifiedTopology: true
+            };
+
+            mongoose.connect(config.app.mongo_uri, options).then(() => resolve(true))
+                .catch((error: Error) => reject(error))
+        })
+    }
+}
+
+export default new Mongo;
