3b52d5d8fc34b336f09b8bb4fd98850f76b51b30
jonasdamher <jonas.damher@gmail.com>
2021-07-17 10:44:27 +0100

    ADD: endpoints para gestionar países

---
+++ src/api/v2/components/countries/countries-model.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+import { Model, model, Document, Schema } from 'mongoose';
+
+export interface Country extends Document {
+    name: string;
+    language: string;
+    ISO: string;
+}
+
+// Para añadir funciones extras con mongoose
+interface countries_model extends Model<Country> { }
+
+const countries_schema: Schema<Country, countries_model> = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: String,
+        required: true
+    },
+    ISO: {
+        type: String,
+        default: null
+    }
+})
+
+export default model('countries', countries_schema);
---
+++ src/api/v2/components/countries/countries-service.ts
@@ -0,0 +1,105 @@
+'use strict';
+
+import COUNTRY, { Country } from './countries-model';
+// Ayudantes
+import { logger_recognitions } from '../../helpers/logger';
+import Text from '../../helpers/Text';
+import response_data from '../../utils/response_data';
+// Tipos
+import Response_data from '../../types/Response_data';
+import { Schema } from 'mongoose';
+
+export {
+    getAll,
+     getWithId,
+     create
+}
+
+function getAll(page: number, perpage: number): Promise<Response_data> {
+    return new Promise((resolve, reject) => {
+
+        const response = response_data();
+
+        let current_page = Math.max(0, page)
+        let pageNum = current_page
+        --current_page
+
+        COUNTRY.find().countDocuments().then((count: any) => {
+
+            const limit = Math.ceil(count / perpage)
+
+            COUNTRY.find({language:'es'})
+                .skip(perpage * current_page)
+                .limit(perpage)
+                .sort('name')
+                .then((authorResponse: any) => {
+
+                    let data = {
+                        countries: authorResponse,
+                        pagination: {
+                            perPage: perpage,
+                            page: pageNum,
+                            lastPage: limit,
+                            total: count
+                        }
+                    };
+
+                    response.result= data;
+                    response.is_valid = true;
+                    resolve(response);
+
+                }).catch((err: any) => {
+
+                    response.status = 400;
+                    response.result= err;
+                    reject(response);
+                });
+
+        }).catch((err: any) => {
+
+            response.status = 400;
+            response.result= err;
+            reject(response);
+        });
+    });
+}
+
+function getWithId(id: string): Promise<Response_data> {
+    return new Promise((resolve, reject) => {
+
+        let response = response_data();
+
+        COUNTRY.findById({ _id: id }).then((poem: any) => {
+            response.is_valid = true;
+            response.result= poem;
+            resolve(response);
+        }).catch((err: any) => {
+            response.status = 400;
+            response.result= err;
+            reject(response)
+        })
+    })
+}
+
+function create(data: any): Promise<Response_data> {
+    return new Promise((resolve, reject) => {
+
+        const response = response_data();
+ 
+        const country: Country = new COUNTRY(data);
+
+        country.save().then((new_poem: Country) => {
+
+            response.status = 201;
+            response.is_valid = true;
+            response.result= new_poem;
+            resolve(response)
+
+        }).catch((err: any) => {
+
+            response.status = 400;
+            response.result= err;
+            reject(response)
+        })
+    })
+}
