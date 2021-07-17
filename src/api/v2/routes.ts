9f6c8cebd0d821f7ea61f0ed72a7974a59e25a6b
jonasdamher <jonas.damher@gmail.com>
2021-07-17 10:29:07 +0100

    UPDATE: endpoints nuevos

---
+++ src/api/v2/routes.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+import express from 'express';
+const router = express.Router()
+// Import routes
+import authors from './components/authors';
+import books from './components/books';
+import editorials from './components/editorials';
+import literary_genres from './components/literary_genres';
+import occupations from './components/occupations';
+import poems from './components/poems';
+import recognitions from './components/recognitions';
+
+import users from './components/users';
+import countries from './components/countries';
+
+router.use('/authors', authors);
+router.use('/books', books);
+router.use('/editorials', editorials);
+router.use('/literary_genres', literary_genres);
+router.use('/occupations', occupations);
+router.use('/poems', poems);
+router.use('/recognitions', recognitions);
+
+router.use('/users', users);
+router.use('/countries', countries);
+
+export default router;
