99f01c40bd1cac585e9a0f912117c1e08a8cae6c
jonasdamher <jonas.damher@gmail.com>
2021-07-19 18:46:36 +0100

    UPDATE: si es develop, muestra console.log

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,11 @@ import { logger_app } from './api/helpers/logger';
 Mongo.connect().then(() => {
     try {
         app.listen(config.app.port);
-        console.log(config.app.url_api);
-
+        
+        if(config.app.node_env==='DEVELOPMENT'){
+            console.log(config.app.url_api);
+        }
+          
     } catch (error: unknown) {
         logger_app.info({ error }, 'Run app')
         process.exit(1);
