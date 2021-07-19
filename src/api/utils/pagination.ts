ee6ed5b1018149c8355a9d37aaf633b3a67c5013
jonasdamher <jonas.damher@gmail.com>
2021-07-19 17:24:25 +0100

    UPDATE: función común para paginaciones

---
+++ src/api/utils/pagination.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+export async function get_pagination(model: any, page: number, perpage: number, data: any = {}) {
+    return new Promise((resolve, reject) => {
+
+        let current_page = Math.max(0, page);
+        let pageNum = current_page;
+        --current_page;
+
+        model.find(data).countDocuments().then((total: any) => {
+
+            const lastPage = Math.ceil(total / perpage)
+
+            resolve({
+                page: pageNum,
+                lastPage: lastPage,
+                perpage: perpage,
+                total: total,
+                page_range: perpage * current_page
+            });
+
+        }).catch((err: any) => {
+            reject(err);
+        });
+    });
+}
