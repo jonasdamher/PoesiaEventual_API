19448ba129af3f4e94ab02e05aa6157ebfa9540f
jonasdamher <jonas.damher@gmail.com>
2021-07-19 17:42:36 +0100

    UPDATE: orientación a objetos

--- src/api/components/users/users-validation.ts
+++ src/api/components/users/users-validation.ts
@@ -5,75 +5,74 @@ import { Request, Response, NextFunction } from 'express';
 import * as regex from '../../utils/regex';
 import { logger_users } from '../../helpers/logger';
 
-export {
-     login,
-    create,
-    update
-}
+class UsersValidation {
 
-async function login(req: Request, res: Response, next: NextFunction) {
-    const schema = Joi.object({
-        email: Joi.string().email().required(),
-        password: Joi.string().required()
-    });
+    async login(req: Request, res: Response, next: NextFunction) {
+        const schema = Joi.object({
+            email: Joi.string().email().required(),
+            password: Joi.string().required()
+        });
 
-    const data = {
-        email: req.body.email,
-        password: req.body.password
+        const data = {
+            email: req.body.email,
+            password: req.body.password
+        }
+
+        schema.validateAsync(data)
+            .then(() => next())
+            .catch((err: Error) => {
+                logger_users.info({ err }, 'validation')
+                return res.status(400).json(err)
+            })
     }
 
-    schema.validateAsync(data)
-        .then(() => next())
-        .catch((err: Error) => {
-            logger_users.info({ err }, 'validation')
-            return res.status(400).json(err)
-        })
-}
+    async create(req: Request, res: Response, next: NextFunction) {
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            lastname: Joi.string().required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().regex(regex.password).required()
+        });
 
-async function create(req: Request, res: Response, next: NextFunction) {
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        lastname: Joi.string().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().regex(regex.password).required()
-    });
+        const data = {
+            name: req.body.name,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password
+        }
 
-    const data = {
-        name: req.body.name,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password
+        schema.validateAsync(data)
+            .then(() => next())
+            .catch((err: Error) => {
+                logger_users.info({ err }, 'validation')
+                return res.status(400).json(err)
+            })
     }
 
-    schema.validateAsync(data)
-        .then(() => next())
-        .catch((err: Error) => {
-            logger_users.info({ err }, 'validation')
-            return res.status(400).json(err)
-        })
-}
+    async update(req: Request, res: Response, next: NextFunction) {
+        const schema = Joi.object({
+            id: Joi.string().hex().required(),
+            name: Joi.string(),
+            lastname: Joi.string(),
+            email: Joi.string().email(),
+            password: Joi.string().regex(regex.password)
+        });
 
-async function update(req: Request, res: Response, next: NextFunction) {
-    const schema = Joi.object({
-        id: Joi.string().hex().required(),
-        name: Joi.string(),
-        lastname: Joi.string(),
-        email: Joi.string().email(),
-        password: Joi.string().regex(regex.password)
-    });
+        const data = {
+            id: req.params.id,
+            name: req.body.name,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password
+        }
 
-    const data = {
-        id: req.params.id,
-        name: req.body.name,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password
+        schema.validateAsync(data)
+            .then(() => next())
+            .catch((err: Error) => {
+                logger_users.info({ err }, 'validation')
+                return res.status(400).json(err)
+            })
     }
-
-    schema.validateAsync(data)
-        .then(() => next())
-        .catch((err: Error) => {
-            logger_users.info({ err }, 'validation')
-            return res.status(400).json(err)
-        })
 }
+
+export default new UsersValidation();
