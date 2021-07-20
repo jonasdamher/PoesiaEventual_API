0f7743c363b5b5543893f718d2c24f9108fe3b89
jonasdamher <jonas.damher@gmail.com>
2021-07-20 09:14:18 +0100

    FIX: método router no devolvia la propiedad router

--- src/api/components/books/index.ts
+++ src/api/components/books/index.ts
@@ -3,22 +3,22 @@
 import express, { Router } from 'express';
 
 import * as validate_common from '../common/common-validate';
-import  validation from './books-validation';
-import  books from './books-controller';
+import validation from './books-validation';
+import books from './books-controller';
 import * as auth from '../../middlewares/auth';
 
 class RouterBooks {
 
-
     private router: Router = express.Router();
 
-    public routes() {
+    public routes(): Router {
         this.router.get('/', validate_common.get_all, books.get_all)
         this.router.get('/:id', validate_common.get_by_id, books.get_by_id)
         this.router.get('/search/:search', validate_common.search, books.search)
         this.router.post('/', auth.user, validation.create, books.create)
-        this.router;
+        return this.router;
     }
 
 }
+
 export default new RouterBooks();
