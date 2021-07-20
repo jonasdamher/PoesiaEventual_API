0d83feba5394c76b6bd4d20eaea52ce5c56447c6
jonasdamher <jonas.damher@gmail.com>
2021-07-20 11:02:18 +0100

    UPDATE: refactorización, limpieza de código

--- src/api/helpers/jwt.ts
+++ src/api/helpers/jwt.ts
@@ -79,57 +79,37 @@ function create_refresh_token(user: any): string {
  * @param USER modelo de tipo de usuario
  * @returns response with object.
  */
-async function refresh_token(
-    refresh_token: string,
-    grant_type: string,
-    USER: any
-) {
+async function refresh_token(refresh_token: string, grant_type: string, USER: any) {
     return new Promise((resolve, reject) => {
 
         if (refresh_token && grant_type === 'refresh_token') {
 
-            jwt.verify(
-                refresh_token,
-                config.jwt.secret_refresh_token,
-                (err: any, data: any) => {
+            jwt.verify(refresh_token, config.jwt.secret_refresh_token, (err: any, data: any) => {
 
-                    if (err) {
-                        reject({ status: 400, message: 'TokenExpired' });
-                    }
+                if (err) reject({ status: 400, message: 'TokenExpired' });
 
-                    const id = data.sub;
-                    const type_token = data.type_token;
+                if (data.type_token !== 'user') {
+                    reject({ status: 400, message: 'BadRequest' });
+                }
 
-                    if (type_token === 'user') {
+                USER.findById(data.sub).select('email name lastname role').then((user: any) => {
 
-                        USER.findById(id).select('email name lastname role').then((user: any) => {
+                    if (!user) reject({ status: 401, message: 'TokenExpired' });
 
-                            if (user) {
+                    const { token, expire_token } = create_token(user, 'user');
 
-                                const { token, expire_token } = create_token(user, 'user');
+                    resolve({
+                        access_token: token,
+                        refresh_token: create_refresh_token(user),
+                        expires_in: expire_token
+                    });
 
-                                resolve({
-                                    access_token: token,
-                                    refresh_token: create_refresh_token(user),
-                                    expires_in: expire_token
-                                });
-
-                            } else {
-                                reject({ status: 401, message: 'TokenExpired' });
-                            }
-
-                        }).catch((err: any) => {
-
-                            reject({ status: 401, message: 'TokenExpired' });
-                        });
-                    } else {
-                        reject({ status: 400, message: 'BadRequest' });
-
-                    }
+                }).catch((err: any) => {
+                    reject({ status: 401, message: 'TokenExpired' });
                 });
-        } else {
-            reject({ status: 400, message: 'BadRequest' });
+            });
         }
+        reject({ status: 400, message: 'BadRequest' });
     });
 }
 
@@ -144,36 +124,21 @@ function confirm_token(token: string) {
         if (token) {
             jwt.verify(token, config.jwt.secret_token, (err: any, data: any) => {
 
-                const { sub, type_token } = data;
+                if (err) reject({ status: 401, message: 'TokenExpired' });
 
-                if (err) {
+                if (data.type_token !== 'user') reject({ status: 400, message: 'BadRequest' });
+
+                USER.findById(data.sub).select('email role').then((user: any) => {
+
+                    if (!user) reject({ status: 401, message: 'TokenExpired' });
+                    resolve(user);
+
+                }).catch((err: any) => {
                     reject({ status: 401, message: 'TokenExpired' });
-                }
-
-                if (type_token === 'user') {
-
-                    USER.findById(sub).select('email role').then((user: any) => {
-
-                        if (user) {
-
-                            resolve(user);
-
-                        } else {
-                            reject({ status: 401, message: 'TokenExpired' });
-                        }
-
-                    }).catch((err: any) => {
-                        reject({ status: 401, message: 'TokenExpired' });
-
-                    });
-                } else {
-                    reject({ status: 400, message: 'BadRequest' });
-                }
-
+                });
             });
-        } else {
-            reject({ status: 400, message: 'BadRequest' });
         }
+        reject({ status: 400, message: 'BadRequest' });
     });
 }
 
@@ -183,39 +148,22 @@ function confirm_token_new_account(token: string) {
         if (token) {
             jwt.verify(token, config.jwt.secret_token, (err: any, data: any) => {
 
-                const { sub, type_token } = data;
+                if (err) reject({ status: 401, message: 'TokenExpired' });
 
-                if (err) {
+                if (data.type_token == 'verify_account') reject({ status: 400, message: 'BadRequest' });
+
+                USER.findById(data.sub).select('verified').then((user: any) => {
+
+                    if (!user) reject({ status: 401, message: 'TokenExpired' });
+                    if (user.verified) reject({ status: 401, message: 'User verified' });
+
+                    resolve(user);
+
+                }).catch((err: any) => {
                     reject({ status: 401, message: 'TokenExpired' });
-                }
-
-                if (type_token === 'verify_account') {
-
-                    USER.findById(sub).select('verified').then((user: any) => {
-
-                        if (user) {
-
-                            if (user.verified) {
-                                reject({ status: 401, message: 'User verified' });
-                            }
-
-                            resolve(user);
-
-                        } else {
-                            reject({ status: 401, message: 'TokenExpired' });
-                        }
-
-                    }).catch((err: any) => {
-                        reject({ status: 401, message: 'TokenExpired' });
-
-                    });
-                } else {
-                    reject({ status: 400, message: 'BadRequest' });
-                }
-
+                });
             });
-        } else {
-            reject({ status: 400, message: 'BadRequest' });
         }
+        reject({ status: 400, message: 'BadRequest' });
     });
 }
